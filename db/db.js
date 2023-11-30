0526fde12d37e09ee9bc302f34c70480007cdd48
simpli-hiren-polra <hiren@simpliigence.com>
2023-11-30 16:29:00 +0530

    warning remove in DB

--- db/db.js
+++ db/db.js
@@ -3,10 +3,7 @@ const mongoose = require("mongoose");
 exports.connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const con = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const con = await mongoose.connect(process.env.MONGO_URI);
     console.log("mongodb is connected", con.connection.host);
   } catch (error) {
     console.log(error);
