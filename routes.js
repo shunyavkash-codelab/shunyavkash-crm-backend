de0d254d9a8288501e0bd4f796ea4ba199e26b37
simpli-hiren-polra <hiren@simpliigence.com>
2023-12-08 10:03:30 +0530

    cors and paginate

--- routes.js
+++ routes.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var cors = require("cors");
 var router = express.Router();
 var clientRouter = require("./routes/client");
 var managerRouter = require("./routes/manager");
@@ -6,6 +7,25 @@ var defaultRoutes = require("./routes/default");
 var projectRouter = require("./routes/project");
 var bankRouter = require("./routes/bank");
 var dashbordRouter = require("./routes/dashboard");
+// Allows cross-origin requests
+var allowedOrigins = ["http://localhost:3000"];
+router.use(
+  cors({
+    credentials: true,
+    origin: function (origin, callback) {
+      // Allow requests with no origin
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  })
+);
 
 // router
 router.use("/client", clientRouter);
