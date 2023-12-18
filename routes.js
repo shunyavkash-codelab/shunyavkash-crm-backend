4b170372820d8641393508e55a2defa001a608e6
simpli-hiren-polra <hiren@simpliigence.com>
2023-12-18 18:19:18 +0530

    countrycode and currency api

--- routes.js
+++ routes.js
@@ -7,6 +7,9 @@ var defaultRoutes = require("./routes/default");
 var projectRouter = require("./routes/project");
 var bankRouter = require("./routes/bank");
 var dashboardRouter = require("./routes/dashboard");
+const Country = require("./model/country");
+const Comman = require("./middleware/comman");
+const Currency = require("./model/currency");
 // Allows cross-origin requests
 var allowedOrigins = ["http://localhost:3000"];
 router.use(
@@ -33,6 +36,28 @@ router.use("/manager", managerRouter);
 router.use("/project", projectRouter);
 router.use("/bank", bankRouter);
 router.use("/dashboard", dashboardRouter);
+
+router.get("/country-code", async (req, res) => {
+  let countryList = await Country.find();
+  Comman.setResponse(
+    res,
+    200,
+    true,
+    "Get country code successfully.",
+    countryList
+  );
+});
+
+router.get("/currency", async (req, res) => {
+  let currencyList = await Currency.find();
+  Comman.setResponse(
+    res,
+    200,
+    true,
+    "Get currency successfully.",
+    currencyList
+  );
+});
 
 // set default routes
 router.use("*", defaultRoutes);
