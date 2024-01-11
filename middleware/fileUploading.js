6e6bda60524c271eb7045b1ad401ead259c3ee90
simpli-hiren-polra <hiren@simpliigence.com>
2024-01-11 19:09:54 +0530

    error handle

--- middleware/fileUploading.js
+++ middleware/fileUploading.js
@@ -12,7 +12,7 @@ exports.fileUploading = async (file) => {
   try {
     let filename = path.join(
       __dirname,
-      "../temp/" +
+      "./temp/" +
         Date.now() +
         "_" +
         Math.ceil(Math.random() * 1e8) +
@@ -30,6 +30,7 @@ exports.fileUploading = async (file) => {
       }
     });
   } catch (error) {
-    return console.log(error);
+    console.log(error);
+    throw error;
   }
 };
