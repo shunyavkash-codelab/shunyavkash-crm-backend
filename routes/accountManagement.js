1cdf9e265e2dff35e4155108b843e9efd33483df
simpli-hiren-polra <hiren@simpliigence.com>
2024-01-16 17:12:56 +0530

    account management

--- routes/accountManagement.js
+++ routes/accountManagement.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 const { add, getAccountList } = require("../controller/v1/accountManagement");
 const errorHandal = require("../middleware/comman").errorHandal;
-const Schema = require("../validationSchema/accountManagementSchema");
 const { getRecord } = require("../middleware/getRecord");
 const accountManagement = require("../model/accountManagement");
 const { authenticateToken, auth } = require("../middleware/verifyToken");
+const Schema = require("../validationSchema/accountManagementSchema.JS");
 
 // create new salary
 router.post(
